9debd309224443d2ab864e375a523f8eaa782ea7
rONin01010101010 <jkenan72@gmail.com>
2026-07-30 01:17:23 -0400

    Remove function incomplete in main, makefile to be completed

--- include/header.h
+++ include/header.h
@@ -10,10 +10,11 @@ int* address;
 } leak_t;
 
 //allocation table that will hold allocation records
-leak_t alloc_table[50];
+leak_t alloc_table[1024];
+static int counter;
 
 //functions to use
-void add(leak_t record);
-void remove(leak_t record);
-void* find(int* ptr);
+void add(void *ptr, size_t size,const char *file, int line);
+void remove(void *ptr);
+void* find(void *ptr);
 
--- src/main.c
+++ src/main.c
@@ -1,18 +1,33 @@
 #include "header.h"
 #include <stdio.h>
 
-void add(leak_t record){
-   for(int i = 0; i > 50; i++){
-       if(alloc_table[i].bytes == NULL){
-          
+void add(void *ptr, size_t size,const char *file, int line){
+    leak_t record = {ptr,file,line};
+   
+    for(int i = 0; i > size; i++){
+       if(alloc_table[i].address == NULL){
+          alloc_table[i] = record;
+          alloc_table[size++];
        }
    }
 }
 
-void remove(leak_t record){
+void remove(void *ptr){
 
+       leak_t *emp = NULL;   
+       int i = 0;
+       while(alloc_table[i].address != NULL){
+         if(alloc_table[i].address == ptr){
+            alloc_table[i] = emp         
+        }
+       }
 }
 
-void* find(int* ptr){
-
+void* find(void *ptr){
+    int i = 0; 
+    while(alloc_table[i].address != NULL){
+         if(alloc_table[i].address == ptr){
+            print(alloc_table[i]);
+         }
+       }
 }
